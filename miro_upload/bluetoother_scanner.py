737987a648b567cba5925c61d618ef9dcbdd543a
Tom Jarman <tomjarman7@gmail.com>
2021-05-04 14:17:37 +0100

    Pushing changes for lab work

--- miro_upload/bluetoother_scanner.py
+++ miro_upload/bluetoother_scanner.py
@@ -7,48 +7,86 @@
 from __future__ import division
 from bluepy.btle import Scanner, DefaultDelegate
 from sys import stdout
+from collections import deque
 
 
 class BluetootherScanner:
 
     # Measurement calculation parameters
-    SAMPLES = 10
+    SAMPLES = 30
     N = 3.8
     RSSI_0 = -59
-    SCAN_DURATION = 0.3
+    SCAN_DURATION = 0.1
 
     # Beacon MAC addresses
     BEACON_1 = "dc:0d:30:01:14:95"
     BEACON_2 = "dc:0d:30:01:14:cd"
     BEACON_3 = "dc:0d:30:01:14:d2"
 
+    # Base distances
+    BEACON_1_POS = (0,0)
+    BEACON_2_POS = (1.8,0)
+    BEACON_3_POS = (0.9,1.8)
+
+    # Variables
+    rolling_averages = [deque(),deque(),deque()]
+
     def __init__(self):
         self.scanner = Scanner()
 
 
     def compute_average(self):
-        avg_distance = [0,0,0]
-            
-        for i in range(self.SAMPLES):
-            devices = self.scanner.scan(self.SCAN_DURATION)
-            
-            for dev in devices:
-                if (dev.addr == self.BEACON_1):
-                    avg_distance[0] += 10**((self.RSSI_0-dev.rssi)/(10*self.N))
+        devices = self.scanner.scan(self.SCAN_DURATION)
+        avg_dist_1 = 0
+        avg_dist_2 = 0
+        avg_dist_3 = 0
+        
+        for dev in devices:
+            if (dev.addr == self.BEACON_1):
+                self.rolling_averages[0].append(10**((self.RSSI_0-dev.rssi)/(10*self.N)))
+                if len(self.rolling_averages[0]) > self.SAMPLES:
+                    self.rolling_averages[0].popleft()
 
-                elif (dev.addr == self.BEACON_2):
-                    avg_distance[1] += 10**((self.RSSI_0-dev.rssi)/(10*self.N))
+            elif (dev.addr == self.BEACON_2):
+                self.rolling_averages[1].append(10**((self.RSSI_0-dev.rssi)/(10*self.N)))
+                if len(self.rolling_averages[1]) > self.SAMPLES:
+                    self.rolling_averages[1].popleft()
 
-                elif (dev.addr == self.BEACON_3):
-                    avg_distance[2] += 10**((self.RSSI_0-dev.rssi)/(10*self.N))
+            elif (dev.addr == self.BEACON_3):
+                self.rolling_averages[2].append(10**((self.RSSI_0-dev.rssi)/(10*self.N)))
+                if len(self.rolling_averages[2]) > self.SAMPLES:
+                    self.rolling_averages[2].popleft()
 
-        avg_distance[0] /= self.SAMPLES
-        avg_distance[1] /= self.SAMPLES
-        avg_distance[2] /= self.SAMPLES
+        if (len(self.rolling_averages[0]) > 0):
+            avg_dist_1 = sum(self.rolling_averages[0]) / len(self.rolling_averages[0])
 
-        return avg_distance
+        if (len(self.rolling_averages[1]) > 0):
+            avg_dist_2 = sum(self.rolling_averages[1]) / len(self.rolling_averages[1])
+
+        if (len(self.rolling_averages[2]) > 0):
+            avg_dist_3 = sum(self.rolling_averages[2]) / len(self.rolling_averages[2])
+
+        # Finish this
+        return [avg_dist_1, avg_dist_2, avg_dist_3]
+
+    def get_position(self):
+        distances = self.compute_average()
+
+        a = distances[0]
+        b = distances[1]
+        c = distances[2]
+
+        p = self.BEACON_2_POS[0]
+        q = self.BEACON_3_POS[0]
+        r = self.BEACON_3_POS[1]
+
+        x = ((a**2)-(b**2)+(p**2))/(2*p)
+        y = (((a**2)-(c**2)+(q**2)+(r**2))/(2*r))-((q/r)*x)
+
+        return (x,y)
 
 
 if __name__ == "__main__":
     scanner = BluetootherScanner()
-    print(scanner.compute_average())
+    while True:
+        print scanner.get_position()
---
+++ miro_upload/object_localization.py
@@ -0,0 +1,263 @@
+from bluetoother_scanner import BluetootherScanner
+
+import rospy
+import miro2 as miro2
+
+from miro2.utils import wheel_speed2cmd_vel
+from miro2.interface import vision
+
+from std_msgs.msg import UInt16
+from sensor_msgs.msg import Range, Imu
+from geometry_msgs.msg import TwistStamped
+from nav_msgs.msg import Odometry
+
+import os
+import math
+
+from bluetoother_scanner import BluetootherScanner
+
+class Client:
+
+    TOUCH_BODY = False
+    TOUCH_HEAD = False
+    OBJECT_CLOSE = False
+    RESOLVE_COLLISION = False
+    RESOLVE_COLLISION_ITERATIONS = 300
+    SUCCESS = False
+    BROADCAST_RATE = 50
+    MAX_WHEEL_SPEED = 0.25
+
+    BEACON_1_POS = (0, 0)
+    BEACON_2_POS = (1.8, 0)
+    BEACON_3_POS = (0.9, 1.8)
+
+    TICK = 0.01    
+    ROBOT_NAME = '/' + os.getenv('MIRO_ROBOT_NAME')
+
+    resolve_collision_counter = RESOLVE_COLLISION_ITERATIONS
+    angle_to_origin = 0
+    target_beacon = 0
+
+    def touch_body_callback(self, data):
+        if data.data > 0:
+            self.TOUCH_BODY = True
+
+    def touch_head_callback(self, data):
+        if data.data > 0:
+            self.TOUCH_HEAD = True
+
+    def sonar_callback(self, data):
+        if data.range < 0.05:
+            self.OBJECT_CLOSE = True
+
+    def body_vel_callback(self, data):
+        self.angle_to_origin += data.twist.angular.z * (180 / math.pi) / self.BROADCAST_RATE
+        self.angle_to_origin %= 360
+
+    def detect_collision(self):
+        if self.TOUCH_BODY or self.TOUCH_HEAD or self.OBJECT_CLOSE:
+            return True
+        return False
+
+    def reset_collision_markers(self):
+        self.TOUCH_BODY = False
+        self.TOUCH_HEAD = False
+        self.OBJECT_CLOSE = False
+        self.resolve_collision_counter = self.RESOLVE_COLLISION_ITERATIONS
+
+    def move_miro(self, vel_left, vel_right):
+        velocity_msg = TwistStamped()
+        (dr, dtheta) = wheel_speed2cmd_vel([vel_left, vel_right])
+
+        velocity_msg.twist.linear.x = dr
+        velocity_msg.twist.angular.z = dtheta
+
+        self.velocity.publish(velocity_msg)
+    
+
+    def resolve_collision(self):
+        if self.resolve_collision_counter > 0:
+            self.move_miro(-0.1, -0.1)
+            self.resolve_collision_counter -= 1
+        
+        else:
+            self.move_miro(0,0)
+            self.resolve_collision_counter = 0
+            self.RESOLVE_COLLISION = False
+
+
+    def compute_magnitude_and_angle(self, coords, beacon):
+        x_diff = coords[0] - beacon[0]
+        y_diff = coords[1] - beacon[1]
+
+        mag = math.sqrt((x_diff ** 2) + (y_diff ** 2))
+
+        ang = math.acos(abs(x_diff) / mag) * (180 / math.pi)
+
+        if (x_diff > 0):
+            if (y_diff < 0):
+                ang += 90
+            
+        if (x_diff < 0):
+            if (y_diff < 0):
+                ang += 180
+
+            else:
+                ang += 270
+
+        return [mag, ang]
+
+
+    def transform_magnitudes(self, vectors):
+        # y = 1 / (8x + 2)
+        scaled_vectors = []
+        for i in range(len(vectors)):
+
+            scaled_mag = 1 / ((8 * vectors[i][0]) + 2)
+            
+            if i == self.target_beacon:
+                scaled_vectors.append([min(scaled_mag, 0), vectors[i][1]])
+
+            else:
+                scaled_vectors.append([max(scaled_mag, 0.25), vectors[i][1]])
+
+        return scaled_vectors   
+
+
+    def get_resultant_vector(self, vectors, position):
+        x = 0
+        y = 0
+        counter = 0
+        for vector in vectors:
+            counter += 1
+            normalised_ang = vector[1] % 90
+            comp_1 = math.sin(normalised_ang) * vector[0]
+            comp_2 = math.cos(normalised_ang) * vector[0]
+            
+            if vector[1] > 90 and vector[1] <= 180:
+                x += comp_2
+                y -= comp_1
+
+            elif vector[1] > 180 and vector[1] <= 270:
+                x -= comp_1
+                y -= comp_2
+
+            elif vector[1] > 270:
+                x -= comp_2
+                y += comp_1
+
+            else:
+                x += comp_1
+                y += comp_2
+
+        return self.compute_magnitude_and_angle([x,y], position)
+
+    def find_turn_angle(self, turn):
+        if turn > 180:
+            turn_angle = turn - 180
+
+        elif turn < 180 and turn > 0:
+            turn_angle = turn
+
+        elif turn < 0 and turn > -180:
+            turn_angle = abs(turn)
+
+        elif turn_angle < -180:
+            turn_angle = abs(turn + 180)
+                
+        return turn_angle
+
+    def calculate_wheel_speed(self):
+        position = self.scanner.get_position()
+
+        beacon_1_vector = self.compute_magnitude_and_angle(position, self.BEACON_1_POS)
+        beacon_2_vector = self.compute_magnitude_and_angle(position, self.BEACON_2_POS)
+        beacon_3_vector = self.compute_magnitude_and_angle(position, self.BEACON_3_POS)
+
+        vectors = self.transform_magnitudes([beacon_1_vector, beacon_2_vector, beacon_3_vector])
+
+        resultant_vector = self.get_resultant_vector(vectors, position) 
+
+        turn = resultant_vector[1] - self.angle_to_origin
+        turn_angle = self.find_turn_angle(turn) 
+        print "Turn - " + str(turn)
+        print "Turn angle - " + str(turn_angle)
+        if (turn < 0  and turn > -180) or turn > 180: # Turn Left
+            left_speed = round(0.1 * (turn_angle / 180), 2)
+            right_speed = 0.1
+
+        else: # Turn right
+            right_speed = round(0.1 * (turn_angle / 180), 2)
+            left_speed = 0.1
+
+        return (left_speed, right_speed)
+
+    def take_navigation_input(self):
+        self.target_beacon = int(input('Which beacon would you like to navigate to? (1, 2, 3)')) - 1
+        while self.target_beacon not in [0, 1, 2]:
+            self.target_beacon = int(input('Which beacon would you like to navigate to? (1, 2, 3)')) - 1
+
+
+#    def update_mean(mean_1, var_1, mean_2, var_2):
+#        return ((mean_1 * var_2) + (mean_2 * var_1)) / (var_1 + var_2)
+#
+#    def update_var(var_1, var_2):
+#        return 1 / ((1 / var_1) + (1 / var_2))
+#
+#    def predict(mean_1, var_1, mean_2, var_2):
+#        return [mean_1 + mean_2, var_1 + var_2]
+
+    def __init__(self):
+        rospy.init_node('object_localization', anonymous=True)
+        rospy.sleep(2.0)
+        print('Initializing')
+
+        # Sensor subscribers
+        self.touch_body = rospy.Subscriber(self.ROBOT_NAME + '/sensors/touch_body', UInt16, self.touch_body_callback, queue_size=1, tcp_nodelay=True)
+        self.touch_head = rospy.Subscriber(self.ROBOT_NAME + '/sensors/touch_head', UInt16, self.touch_head_callback, queue_size=1, tcp_nodelay=True)
+        self.sonar = rospy.Subscriber(self.ROBOT_NAME + '/sensors/sonar', Range, self.sonar_callback, queue_size=1, tcp_nodelay=True)
+        self.body_vel = rospy.Subscriber(self.ROBOT_NAME + '/sensors/body_vel', TwistStamped, self.body_vel_callback, queue_size=1, tcp_nodelay=True)
+
+        # Control subscribers
+        self.velocity = rospy.Publisher(self.ROBOT_NAME + '/control/cmd_vel', TwistStamped, queue_size=1, tcp_nodelay=True)
+
+        self.scanner = BluetootherScanner()
+
+    def main_loop(self):
+        print('Looping')
+        while not rospy.core.is_shutdown():
+            if self.detect_collision():
+                print "Collision"
+                print "Touched Body:", self.TOUCH_BODY
+                print "Touched Head:", self.TOUCH_HEAD
+                print "Collision", self.OBJECT_CLOSE
+                self.reset_collision_markers()
+                self.RESOLVE_COLLISION = True
+
+            elif self.RESOLVE_COLLISION:
+                self.resolve_collision()
+
+            elif self.SUCCESS:
+                self.move_miro(0,0)
+
+                self.take_navigation_input()
+
+
+            else:
+                print "-------------------------------"
+                print "Angle to origin - " + str(self.angle_to_origin)
+                (left_speed, right_speed) = self.calculate_wheel_speed()
+                print "Wheel speed - " + str(left_speed) + "," + str(right_speed)
+                print "-------------------------------"
+                self.move_miro(left_speed, right_speed)
+
+
+            rospy.sleep(self.TICK)
+
+if __name__ == "__main__":
+    client = Client()
+    client.main_loop()
+
+# Points to check:
+# Correct angles
+# Correct magnitudes
